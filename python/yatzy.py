a7b9dfc54bfe6dcc1e12c6ba754352918bd71c7e
DanyZurita <dzurita@cifpfbmoll.eu>
2019-12-05 19:13:02 +0100

    refactor(yatzy): Refactorizar la funcion yatzy
    Refactorizar la funcion jatzy con un for y un if

--- python/yatzy.py
+++ python/yatzy.py
@@ -9,13 +9,12 @@ class Yatzy:
 
     @staticmethod
     def yatzy(dice):
-        counts = [0]*(len(dice)+1)
         for die in dice:
-            counts[die-1] += 1
-        for i in range(len(counts)):
-            if counts[i] == 5:
-                return 50
-        return 0
+            value = dice[0]
+            value -= die
+            if value != 0:
+                return 0
+        return 50
 
     @staticmethod
     def ones(d1,  d2,  d3,  d4,  d5):
