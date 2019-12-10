5e87bc41484e7c94a038ac34bb8b633c9d36e7af
Albertomanas <amanas@cifpfbmoll.eu>
2019-12-10 19:12:18 +0100

    refactor(yatzy.py refactoring): Refactoring the def full_house

--- python/yatzy.py
+++ python/yatzy.py
@@ -131,32 +131,9 @@ class Yatzy:
         return 20
 
     @staticmethod
-    def full_house(d1,  d2,  d3,  d4,  d5):
-        tallies = []
-        _2 = False
-        i = 0
-        _2_at = 0
-        _3 = False
-        _3_at = 0
-
-        tallies = [0]*6
-        tallies[d1-1] += 1
-        tallies[d2-1] += 1
-        tallies[d3-1] += 1
-        tallies[d4-1] += 1
-        tallies[d5-1] += 1
-
-        for i in range(6):
-            if (tallies[i] == 2):
-                _2 = True
-                _2_at = i+1
-
-        for i in range(6):
-            if (tallies[i] == 3):
-                _3 = True
-                _3_at = i+1
-
-        if (_2 and _3):
-            return _2_at * 2 + _3_at * 3
-        else:
-            return 0
+    def full_house(*dices):
+        number_list = []
+        for die in dices:
+            if dices.count(die) in (2, 3):
+                number_list.append(die)
+        return sum(number_list)
